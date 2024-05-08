297d657a4e4de9286eb65552a7b85bfb1b2b34ef
LeandraDS <santosleandra@prof.educacao.sp.gov.br>
2024-05-08 13:22:47 -0300

    Projeto original js

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let palavra
+function palavraAleatoria(){
+  let palavras = ["Tecnologia", "JavaScript", "Programação", "Alura", "Robótica", "p5js", "Escola"]
+    return random(palavras)}
+  
+  function setup(){
+  createCanvas(600,400)
+  
+  palavra = palavraAleatoria()}
+  
+ function inicializaCores(){
+   background("#07011B")
+   fill("#fff")
+   textSize(64)
+   textAlign(CENTER,CENTER)}
+  
+  function draw(){
+    inicializaCores()
+    let maximo = width
+    let minimo = 0
+    let quantidade = map(mouseX,0,width,0,palavra.length)
+    if(quantidade<1){
+      palavra = palavraAleatoria()}
+    
+  let parcial = palavra.substring(0,quantidade)
+  text(parcial,300,200)
+  }
